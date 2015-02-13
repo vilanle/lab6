5028c15ab8abf2927b72eb244232d7aaf3cb40ac
vagrant <vagrant@precise32.(none)>
2015-02-13 19:35:05 +0000

    ajax lab

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var index = require('./routes/index');
 var project = require('./routes/project');
 // Example route
 // var user = require('./routes/user');
+var palette = require('./routes/palette')
 
 var app = express();
 
@@ -38,6 +39,7 @@ if ('development' == app.get('env')) {
 // Add routes here
 app.get('/', index.view);
 app.get('/project/:id', project.projectInfo);
+app.get('/palette', palette.randomPalette);
 // Example route
 // app.get('/users', user.list);
 
--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,6 +27,32 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+	$.get("/project/" + idNumber,
+		function(result){
+			$(".project").click(function(){
+				// Prevent following the link
+				e.preventDefault();
+
+				// Get the div ID, e.g., "project3"
+				var project = $(this).closest('.project');
+				// get rid of 'project' from the front of the id 'project3'
+				var detail = $(project).find(".details");
+
+				var projectHTML = '<a href="#" class="thumbnail">' +
+				    '<img src="' + result['image'] + '" class="detailsImage">' +
+				    '<p>' + result['title'] + '</p>' +
+				    '<p><small>' + result['date'] +
+				    '</small></p></a>';
+
+				$(detail).html(projectHTML);
+
+			  	console.log(projectHTML);
+			})
+
+	});
+
+	console.log("/project/" + idNumber);
+
 }
 
 /*
@@ -34,5 +60,23 @@ function addProjectDetails(e) {
  * and apply it
  */
 function randomizeColors(e) {
+
+	e.preventDefault();
+
+	$.get("/palette", function(result){
+		//console.log("hi");
+		var colors = result.colors.hex;
+
+
+		$('body').css('background-color', colors[0]);
+		$('.thumbnail').css('background-color', colors[1]);
+		$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+		$('p').css('color', colors[3]);
+		$('.project img').css('opacity', .75);
+
+	})
+
+
+
 	console.log("User clicked on color button");
 }
